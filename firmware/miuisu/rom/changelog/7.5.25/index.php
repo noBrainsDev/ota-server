8b69d324940ffb2b3a7835f9352643e0bbe88296
iSm1le <sm1leua@ya.ru>
2017-06-05 07:37:49 +0300

    init commit

---
+++ firmware/miuisu/rom/changelog/7.5.25/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+    <head>
+        <meta name='HandheldFriendly' content='true' />
+        <!-- meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' /-->
+        <meta http-equiv='Content-Type' content='text/html;charset=utf-8' />
+        <style type='text/css'>
+           body{margin: 0px;padding: 10px;background-color:transparent;font-family:Droid Sans Fallback;font-size:16px;}
+            #mainbox{position:absolute;width:312px;margin:0px;padding:0px;border-style:dotted;border-width:0px;}
+            img{padding:0;margin:0; border:0;}
+            #footer {margin-top:40px;color: grey;text-align: center;}
+            #footer h3 {margin: 5px;font-size: 12px;}
+            .image{width: 280px;}
+            ul {padding-left: 22px;list-style-position: outside;margin:0;}
+			.tuijian h2{margin-left:-24px;}
+            li {margin-bottom: 10px;line-height:22px;color:#666666;list-style:none outside url(http://rommgr.miui.com/images/point_2.png);}
+            h1 {font-size: 20px;}
+            h2{color:#ff7e00;line-height:28px;border-bottom:2px solid #d5d5d5;font-size:18px;padding:0 29px 16px;}
+			.h2_1{background:url(http://rommgr.miui.com/images/icon_7.png) no-repeat 3px 0px;}
+			.h2_2{background:url(http://rommgr.miui.com/images/icon_8.png) no-repeat 2px 0px;}
+			.tuijian{position:relative;}
+			.tuijiandu{color: #666666;height: 28px;line-height: 28px;/*position: absolute;*/float: right;right:12px;text-align: right;top: 25px;width: 150px;margin:0;}
+			h3{margin:15px 20px;}
+			a{color:#236b99;text-decoration:none;}
+			a{color:#236b99;text-decoration:none;}
+            img{width:100%;}
+			#video{width:100%;display:none;}
+			.line1{border-bottom:1px solid #d5d5d5;height:10px;}
+			.line{height:1px;background:url(http://rommgr.miui.com/images/dump_line.jpg) repeat-x scroll ;opacity:0.5;}	
+        </style>
+    </head>
+    <body>
+        <div id="update">
+            <h2 class="h2_2">Список изменений, лол</h2><h3>Экран блокировки/Панель уведомлений</h3><ul><li><span style="color: #ff0000;"><strong>[Исправлено]</strong></span> Иногда обои экрана блокировки не отображались (05-23)</li><li><span style="color: #ff0000;"><strong>[Исправлено]</strong></span> Иконка 4G+ не отображалась в строке состояния (05-23)</li></ul><h3>Рабочий стол</h3><ul><li><span style="color: #ff0000;"><strong>[Исправлено]</strong></span> Ярлык очистки не работал (05-23)</li></ul><h3>Погода</h3><ul><li><span style="color: #0000ff;"><strong>[Оптимизировано]</strong></span> Можно добавить до 10 городов (05-23)</li></ul><h3>Настройки</h3><ul><li><span style="color: #ff0000;"><strong>[Исправлено]</strong></span> Исправлено воспроизведение звукового уведомления от клонированного приложения WhatsApp (05-24)</li></ul><h3>Отчеты</h3><ul><li><span style="color: #0000ff;"><strong>[Оптимизировано]</strong></span> Новый интерфейс и категории (05-24)</li></ul>              
+        </div>
+    </body>
+</html>
